af7ac0aefb30bc5d199ce17d9ff2f3b69bb1b261
Tor <tortb@stud.ntnu.no>
2019-02-12 13:05:26 +0100

    Created frontend-branch

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="index">
-    <h1>This is an about page</h1>
+    <h1>Velkommen til siden</h1>
+    <button class="" ></button>
   </div>
 </template>
+
+
+<style>
+html, body{
+
+  background-color:#f2f2f2;
+
+  font-family: Roboto;
+}
+
+</style>
