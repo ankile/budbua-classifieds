bbef2396f77ad671736e6403943b2d1f0634eb7a
ivarnm <36505347+ivarnm@users.noreply.github.com>
2019-02-15 12:45:35 +0100

    Added adSearch feature

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from './views/Index.vue'
 import Details from "./views/ad/Details";
+import About from "./views/About";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/details/:id',
       name: 'details',
       component: Details
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     }
   ]
 })
