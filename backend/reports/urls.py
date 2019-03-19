781fbf4dd8a29d8fdeb4d0a4c734819f56521f50
kentare <kentat@stud.ntnu.no>
2019-03-19 11:34:18 +0100

    feature: add bid url structure

--- backend/reports/urls.py
+++ backend/reports/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TestView.as_view(), name='rating'),
-
+    path('ads/<int:pk>/', views.AdsReportView.as_view(), name='ads-report'),
+    path('users/<int:pk>/', views.UsersReportView.as_view(), name='user-report'),
 ]
