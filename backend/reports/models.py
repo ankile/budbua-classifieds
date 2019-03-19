2f214f107624cb10d66cbf3d01daa6fe554cb6b7
kentare <kentat@stud.ntnu.no>
2019-03-19 12:40:41 +0100

    feature: add user/add model

--- backend/reports/models.py
+++ backend/reports/models.py
@@ -1,3 +1,49 @@
+
 from django.db import models
 
-# Create your models here.
+from budbua.utils.mixins import TimeStampable
+from django.core.exceptions import ValidationError
+from users.models import User
+from auctions.models import Ad
+
+
+class AdReport(TimeStampable):
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return f'AdReport(reporter={self.reporter}, ad="{self.ad}")'
+
+    reporter = models.ForeignKey(
+        User,
+        verbose_name='reporter',
+        on_delete=models.CASCADE
+    )
+
+    ad = models.ForeignKey(
+        Ad,
+        verbose_name='ad',
+        on_delete=models.CASCADE
+    )
+
+
+class UserReport(TimeStampable):
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return f'UserReport(reporter={self.reporter}, reported="{self.reported}")'
+
+    reporter = models.ForeignKey(
+        User,
+        verbose_name='reporter',
+        on_delete=models.CASCADE
+    )
+
+    reported = models.ForeignKey(
+        User,
+        verbose_name='reported',
+        on_delete=models.CASCADE
+    )
