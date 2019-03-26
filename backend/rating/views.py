15a374d3c58d48376bb0020ceb574e2aaad25191
nic <nicobrumm@hotmail.com>
2019-03-26 11:40:29 +0100

    feature: Add buyer/seller rating

--- backend/rating/views.py
+++ backend/rating/views.py
@@ -1,20 +1,33 @@
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Rating
+
 # from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import RatingSerializer
 
 
-class TestView(APIView):
+class RatingView(APIView):
+    def post(self, request):
+        serializer = RatingSerializer(data={
+            'rating':request.data.get('rating'),
+            'rating_giver': request.user.id,
+            'rating_receiver': request.GET.get('ratingReceiver'),
+        })
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request):
+        rating = Rating.objects.get(rating_receiver=request.GET.get('ratingReceiver'), rating_giver=request.user.id)
+        serializer = RatingSerializer(rating, data={**request.data, 'rating_giver': request.user.id}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK)
 
     def get(self, request):
-        print(request)
-        return Response({'Test': 'this works "rating"'}, status=status.HTTP_200_OK)
-
-
-class RatingCreateView(APIView):
-    def post(self, request):
-        serializer = RatingSerializer(data={**request.data, 'rating_giver':request.user.id})
-        serializer.is_valid(raise_exception=True)
-        serializer.create(serializer.validated_data)
-        return Respons(serializer.data, status=status.HTTP_201_CREATED)
+        rating = Rating.objects.filter(rating_receiver=request.GET.get('ratingReceiver'), rating_giver=request.user.id)
+        if rating.exists():
+            serializer = RatingSerializer(rating.get())
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_204_NO_CONTENT)
