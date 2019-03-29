22d5cdb7a5ffb886cc855761bc27e146de7593c0
Tor <tortb@stud.ntnu.no>
2019-03-29 14:39:00 +0100

    Merge branch 'dev' of https://gitlab.stud.idi.ntnu.no/programvareutvikling-v19/gruppe-7 into 22-frontend-message-other-users

--- frontend/src/views/ad/DetailsGeneral.vue
+++ frontend/src/views/ad/DetailsGeneral.vue
@@ -5,48 +5,51 @@
 -->
 
 <template>
-  <div class="general">
-    <h1 is="sui-header" class="title">{{ad.title}}</h1>
-    <img v-bind:src="this.ad.imageString" alt="Annonsebilde">
-    <article>{{ad.description}}</article>
-    <sui-divider section></sui-divider>
+    <div class="general">
+        <h1 is="sui-header" class="title">{{ad.title}}</h1>
+        <img v-bind:src="this.ad.imageString" alt="Annonsebilde">
+        <article>{{ad.description}}</article>
+        <sui-divider section></sui-divider>
 
-    <sui-grid :columns="3" divided stackable>
-        <sui-grid-column>
-            <h3 v-if="timeremaining">
-                <sui-loader v-if="timeremaining.includes('NaNd')" active centered inline />
-                <div v-else> Utløper om:
-                {{timeremaining}}
-                </div>
-            </h3>
-            <h3 class="expired" v-else>Utløpt</h3>
-        </sui-grid-column>
-        <sui-grid-column>
-          <h3>Selger: {{ad.firstName}} {{ad.lastName}}</h3>
-          <sui-label v-if="ad.userRating">
-            <sui-icon name="star" color="yellow"/> {{ad.userRating}} / 5
-          </sui-label>
-          <sui-label v-else>
-            <sui-icon name="star" color="yellow"/> Ingen vurderinger enda
-          </sui-label>
-            <sui-button basic positive class="details__msg_button" v-on:click="createChat">Send melding til selger</sui-button>
-        </sui-grid-column>
-        <sui-grid-column>
-          <h3 class="highest-bid" v-if="ad.maximumBid">
-              <div v-if="user && ad.highestBidder.id == user.id">
-                  Du leder budet!<br/>
-                  Høyeste bud: {{ad.maximumBid}} kr.
-              </div>
-              <div v-else>
-                  Leder av budet: {{ad.highestBidder.name}}<br/>
-                  Høyeste bud: {{ad.maximumBid}} kr.
-              </div>
-          </h3>
-          <h3 class="highest-bid" v-else>Ingen bud lagt inn <br>
-            Minimum bud: {{ad.minimumBid}} kr</h3>
-        </sui-grid-column>
-    </sui-grid>
-  </div>
+        <sui-grid :columns="3" divided stackable>
+            <sui-grid-column>
+                <h3 v-if="timeremaining">
+                    <sui-loader v-if="timeremaining.includes('NaNd')" active centered inline/>
+                    <div v-else> Utløper om:
+                        {{timeremaining}}
+                    </div>
+                </h3>
+                <h3 class="expired" v-else>Utløpt</h3>
+            </sui-grid-column>
+            <sui-grid-column>
+                <h3>Selger: {{ad.firstName}} {{ad.lastName}}</h3>
+                <sui-label v-if="ad.userRating">
+                    <sui-icon name="star" color="yellow"/>
+                    {{ad.userRating}} / 5
+                </sui-label>
+                <sui-label v-else>
+                    <sui-icon name="star" color="yellow"/>
+                    Ingen vurderinger enda
+                </sui-label>
+                <sui-button basic positive class="details__msg_button" v-on:click="createChat">Send melding til selger
+                </sui-button>
+            </sui-grid-column>
+            <sui-grid-column>
+                <h3 class="highest-bid" v-if="ad.maximumBid">
+                    <div v-if="user && ad.highestBidder.id == user.id">
+                        Du leder budet!<br/>
+                        Høyeste bud: {{ad.maximumBid}} kr.
+                    </div>
+                    <div v-else>
+                        Leder av budet: {{ad.highestBidder.name}}<br/>
+                        Høyeste bud: {{ad.maximumBid}} kr.
+                    </div>
+                </h3>
+                <h3 class="highest-bid" v-else>Ingen bud lagt inn <br>
+                    Minimum bud: {{ad.minimumBid}} kr</h3>
+            </sui-grid-column>
+        </sui-grid>
+    </div>
 </template>
 
 
@@ -68,12 +71,13 @@
                 timeremaining: undefined
             }
         },
-        methods:{
-            createChat(){
+        methods: {
+            createChat() {
                 MessageApi.createChat(this.ad.owner)
-                    .then(res=>{
-                        router.push('/messages/?id='+res.data.id)
+                    .then(res => {
+                        router.push('/messages/?id=' + res.data.id)
                     })
+
             }
         },
         created() {
@@ -90,28 +94,28 @@
 </script>
 
 
-
 <style scoped>
 
-  .general {
-    padding: 3rem 0;
-  }
+    .general {
+        padding: 3rem 0;
+    }
 
-  img {
-    max-width: 100%;
-    max-height: 100%;
-    box-shadow: 0 0 8px #1e639d;
-  }
+    img {
+        max-width: 100%;
+        max-height: 100%;
+        background-color: white;
+        border-radius: 5px;
+    }
 
-  article {
-    margin-top: 2rem;
-  }
+    article {
+        margin-top: 2rem;
+    }
 
-  .expired {
-    color: red;
-  }
+    .expired {
+        color: red;
+    }
 
-    .details__msg_button{
-        margin:10px !important;
+    .details__msg_button {
+        margin: 10px !important;
     }
 </style>
