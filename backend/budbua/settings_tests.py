e1d32c8319ba8397872ee54b9ec9e8095a5e3cb0
kentare <kentat@stud.ntnu.no>
2019-04-08 11:54:03 +0200

    fix: add allowed hosts

--- backend/budbua/settings_tests.py
+++ backend/budbua/settings_tests.py
@@ -4,16 +4,37 @@ import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 DEBUG = True
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
 )
 
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
+
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=365)
 }
 
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'users',
+    'auctions',
+    'analytics',
+    'reports',
+    'rating',
+    'user_messages',
+]
 
 DATABASES = {
     'default': {
